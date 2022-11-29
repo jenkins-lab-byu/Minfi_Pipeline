7ec4a0a47d81f1f38dc85068a812aee20d9c2289
chadapollard <84200246+chadapollard@users.noreply.github.com>
2022-11-29 09:21:44 -0700

    Update minfi_pipeline.sh

--- minfi_pipeline.sh
+++ minfi_pipeline.sh
@@ -1,17 +1,18 @@
+
 ### Pathnames to change before running pipeline
 # Pathname to the minfi pipeline directory that was downloaded from github. (Make sure to include a '/' at the end')
 script_base=/Volumes/Research_Data/Jenkins_Lab_Github/Minfi_Pipeline/
 
 # Pathname to the basedirectory that contains all files relevant to the study
-base_dir="/Volumes/Research_Data/Research_Datasets/Atherosclerosis_Tissue/Atherosclerotic_Plaques_450K"
+base_dir="/Users/chadpollard/Downloads/Air_Quality"
 
 #Name of the basedirectory that contains all files relevant to the study (Make sure it is just the name of the directory, not the pathname)
-name=Atherosclerotic_Plaques_450K
+name=Air_Quality
 
 # Name of the Sample Sheet inside your basedirectory that contains all the sample metadata. (Make sure the first three columns are 'Sample_Name','Sentrix_ID','Sentrix_Position')
 sample_sheet=sample_sheet.csv
 
-
+# You dont need to touch anything after this point!!!!
 
 # Pathname to the basedirectory that contains all 
 additional_files_base="${script_base}additional_files/"
